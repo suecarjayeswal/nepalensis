17a59c893163e5b194f7e57ef843bb9f8fe49b6d
suecarjayeswal <suecarjayeswal098@gmail.com>
2022-09-04 15:34:02 +0545

    location and marker added

--- BOLDmapNepal/map/views.py
+++ BOLDmapNepal/map/views.py
@@ -3,7 +3,8 @@ import folium
 # Create your views here.
 def index(request):
     #creating map object
-    m = folium.Map(location=[])
+    m = folium.Map(location=[27.79,85.2714],zoom_start=6)
+    folium.Marker([27.79,85.2714],tooltip='Click for More',popup='Bagmati').add_to(m)
     # get html repr of map object
     m = m._repr_html_()
     context = {
